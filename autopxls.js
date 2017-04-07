ea52a94e81e2214b59e7d48a943860d59f284925
ED <des.binc@gmail.com>
2017-04-08 01:28:54 +0300

    ignore transparent pixels

--- autopxls.js
+++ autopxls.js
@@ -21,7 +21,7 @@ function AutoPXLS(images){
       var board_pixel = board.getImageData((parseInt(x) + parseInt(coords["x"])), (parseInt(y) + parseInt(coords["y"])), 1, 1).data;
       var image_pixel = image.getImageData(coords["x"], coords["y"], 1, 1).data;
 
-      if(image_pixel[3] <=127 0) return true;
+      if(image_pixel[3] <= 127) return true;
 
       for(var i = 0; i < 3; i++){
         if(board_pixel[i] != image_pixel[i]) return false;
