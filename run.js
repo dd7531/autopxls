203f98898e357e7b9f6667ac106d9914beeacec9
ED <des.binc@gmail.com>
2017-04-08 06:21:56 +0300

    runner update

--- run.js
+++ run.js
@@ -1,30 +1,30 @@
 // ============================================= configure and run ============
+function autopxls_loader(){
+  var images = [
+    {
+      title: "TARDIS",
+      x: 602,
+      y: 463,
+      image: "http://i.imgur.com/wrpOqWw.png" 
+    },
+    {
+      title: "Napoleon RIP",
+      x: 440,
+      y: 574,
+      image: "http://i.imgur.com/uot63Pq.png" 
+    },
+    {
+      title: "El Gagarino",
+      x: 60,
+      y: 526,
+      image: "http://i.imgur.com/mxjxBhS.png"
+    }
+  ]
 
-var images = [
-  {
-    title: "TARDIS",
-    x: 602,
-    y: 463,
-    image: "http://i.imgur.com/wrpOqWw.png" 
-  },
-  {
-    title: "Napoleon RIP",
-    x: 440,
-    y: 574,
-    image: "http://i.imgur.com/uot63Pq.png" 
-  },
-  {
-    title: "El Gagarino",
-    x: 60,
-    y: 526,
-    image: "http://i.imgur.com/mxjxBhS.png"
-  }
-]
-
-var autopxls_script = document.createElement('script');
-autopxls_script.src = "https://rawgit.com/dd7531/autopxls/master/autopxls.js";
-autopxls_script.onload = function () {
-  AutoPXLS(images);
-};
- 
-document.head.appendChild(autopxls_script);
+  var autopxls_script = document.createElement('script');
+  autopxls_script.src = "https://rawgit.com/dd7531/autopxls/master/autopxls.js";
+  autopxls_script.onload = function () {
+    AutoPXLS(images);
+  };
+  document.head.appendChild(autopxls_script);
+}
