67122ca2d2cf76b393d674a463999053b1b4017e
ED <des.binc@gmail.com>
2017-04-08 03:31:26 +0300

    remove testing data

--- autopxls.js
+++ autopxls.js
@@ -157,23 +157,3 @@ function AutoPXLS(images){
 
   draw();
 }
-
-
-// ============================================= configure and run ============
-
-var images = [
-  {
-    title: "TARDIS",
-    x: 602,
-    y: 463,
-    image: "http://i.imgur.com/wrpOqWw.png" 
-  },
-  {
-    title: "Napoleon RIP",
-    x: 440,
-    y: 574,
-    image: "http://i.imgur.com/uot63Pq.png" 
-  }
-]
-
-AutoPXLS(images);
